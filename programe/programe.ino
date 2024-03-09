ac9f79a51aabc827c59df76b24c16a9f59a9e58b
roipicsou <pl613330@gmail.com>
2024-03-09 20:50:57 +0100

    creation des comentair pour avoir lidée du code

---
+++ programe/programe.ino
@@ -0,0 +1,25 @@
+void setup() {
+  
+}
+
+void homme_mor_day(){
+  // verifie si conection internette est etablie
+  // si oui envoi un mail
+  // atent la desactivation par mail ou par rfid
+  // si aucun signe de vie detruit toute information
+}
+
+void homme_mor_moth(){
+  // verifie si conection internette est etablie
+  // si oui envoi un mail
+  // atent la desactivation par mail ou par rfid
+  // si aucun signe de vie detruit toute information
+  // si signe de vie genere une nouvelle clée
+}
+
+void loop() {
+  // verification du temp avant homme mort
+  // si le temp est inferieur a 10 min et que 30 ne se sont paq ecouler apelle de la fonction homme_mor_day
+  // si le temp est inferieur a 10 min et que 30 se sont paq ecouler apelle de la fonction homme_mor_moth
+  // sino apelle la fonction qui fait tourner la bras de 1°
+}
